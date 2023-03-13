6c2f8d44ab042ed063748d89e0899bd33ed4ca50
neilk17 <kanakianeil@gmail.com>
2023-03-12 21:03:28 -0400

    ♻️ restructure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
         'light-red': 'hsl(0, 100%, 67%)',
         'orangey-yellow': 'hsl(39, 100%, 56%)',
         'green-teal': 'hsl(166, 100%, 37%)',
-        'cobalt-blue':'hsl(234, 85%, 45%)',
-        'light-slate-blue' : 'hsl(252, 100%, 67%)',
-        'light-royal-blue':'hsl(241, 81%, 54%)',
-        'violet-blue' : 'hsla(256, 72%, 46%, 1)',
-        'persian-blue' : 'hsl(241, 72%, 46%)',
+        'cobalt-blue': 'hsl(234, 85%, 45%)',
+        'light-slate-blue': 'hsl(252, 100%, 67%)',
+        'light-royal-blue': 'hsl(241, 81%, 54%)',
+        'violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'persian-blue': 'hsl(241, 72%, 46%)',
         'white': 'hsl(0, 0%, 100%)',
         'pale-blue': 'hsl(221, 100%, 96%)',
         'light-lavender': 'hsl(241, 100%, 89%)',
